0c638ba95cd5c75abeb7fd2ce6410578c8e6337d
André Ovidio <andreovidio55@gmail.com>
2026-03-25 22:48:03 -0300

    Update project base path for Vite and switch logo format; remove social icons from footer.

---
+++ assets/logo-0-CowKrhFx.js
@@ -0,0 +1,1 @@
+const o=""+new URL("logo-BNlCdhZS.jpeg",import.meta.url).href;export{o as default};
--- assets/logo-0-DJBxwVKQ.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("logo-0-BNlCdhZS.jpeg",import.meta.url).href;export{o as default};
